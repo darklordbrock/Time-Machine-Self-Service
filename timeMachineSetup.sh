2c8f412d010dcdfd68de4dc4adaad1a387744a98
Kyle Brockman <kyle@Kyles-Retina-MBPro.local>
2013-12-04 11:50:12 -0600

    A lot of changes
    Changed very thing over to CocoaDialog(CD). The first window uses the
    fileselect from CD, in a while loop. This check that it is a local disk
    of the computer.
    
    Also the Script is still in testing mood.

--- timeMachineSetup.sh
+++ timeMachineSetup.sh
@@ -14,204 +14,150 @@
 CD="/Applications/Utilities/CocoaDialog.app/Contents/MacOS/CocoaDialog"
 CDI="/Applications/Utilities/CocoaDialog.app/Contents/Resources"
 
-#YES=`echo "button returned:Yes"`
-#NO=`echo "button returned:No"`
+TMD="NotADisk"
 
-#echo $YES "test1"
-#echo $NO "test2"
-
-#applescipt to ask which drive to enable Time Machine on. 
-
-#TMD=`/usr/bin/osascript <<-EOF
-#set fileLists to paragraphs of (do shell script "ls /Volumes/ | grep -v 'Macintosh HD' | grep -v 'JSS_Repo'")
-#    tell application "System Events"
-#        activate
-#        choose from list fileLists
-#    end tell
-#EOF`
-
-echo $TMD
-
-#check if the user clicks cancel, if cancel clicked the script quits 
-
-if [ "$TMD" == "false" ] ; then
-	echo "User clicked Cancel"
-	exit 1
+while [ $TMD = "NotADisk" ];do
 	
+	TMD=`$CD fileselect --select-only-directories --float --with-directory /Volumes --text "Select the Backup Drive for Time Machine"`
+	
+	echo $TMD
+	TMDselected=`echo $TMD`
+	TMD1=`echo $TMD | awk '{ print $1 }'`
+	
+	if [ "$TMD1" == "" ] ; then
+		echo "User clicked Cancel"
+		exit 1
+	fi
+	echo $TMDRECORD "in while loop"
+	mount | grep "$TMDselected" | grep local
+	if [ $? != "0" ]; then
+		$CD ok-msgbox --no-cancel --icon hazard --float --text "Your selection is not a usable drive for Time Machine." --informative-text "Please select again"
+		TMD="NotADisk"
+	fi
+	
+done
+
+echo "TMD1" $TMD1
+echo "TMDselected" $TMDselected
+echo "User Selected " $TMDselected " as their Time Machine Drive"
+
+#
+TMDRECORD=`diskutil info "$TMDselected" | grep "Partition Type:" | awk '{print $3}'`
+TMDFORMAT=`diskutil info "$TMDselected" | grep "File System Personality:" | awk '{print $4 $5}'`
+DISK=`diskutil info "$TMDselected" | grep "Part of Whole:" | awk '{print $4}'`
+COREDISK=`diskutil corestorage info $DISK | grep "Conversion Status:" | awk '{print $3}'`
+
+#
+echo "Time Machine Drive Selected Partition Type: " $TMDRECORD 
+
+echo "Time Machine Drive Selected File System Personality: " $TMDFORMAT
+
+echo "Time Machine Drive Selected Part of Disk: " $DISK
+
+#
+if [ "$COREDISK" == "Complete" ]; then
+	echo "Disk is already encrypted"
+	
+	echo "tmutil setdestination"
+	#tmutil setdestination /Volumes/$TMDselected
+
+	echo "tmutil enable for automatic backups"
+	#tmutil enable
+
+	echo "tmutil enable local snapshots"
+	#tmutil enablelocal
+	
+	exit 0
+else
+	echo "Disk is not already encrypted"
+fi
+
+#
+if [ "$TMDRECORD" != "Apple_HFS" ] ; then
+	
+		#ask the users are they really sure
+		FIRST=`$CD yesno-msgbox --no-cancel --string-output --no-newline --text "The drive you have selected needs to be re-formatted to be used with Time Machine. Do you want to re-format it?"`
+
+		if [ "$FIRST" == "YES" ]; then
+			echo "Uesr clicked yes first time"
+		else
+			echo "User clicked no first time"
+			exit 1
+		fi
+
+		#do you want me to format X
+	
+		SECOND=`$CD yesno-msgbox --no-cancel --string-output --no-newline --text "All data on this drive will be lost when it is re-formatted. Are you sure you want it re-formatted?"`
+
+		if [ "$SECOND" == "YES" ]; then
+			echo "Uesr clicked yes second time"
+		else
+			echo "User clicked no second time"
+			exit 1
+		fi
+
+		#are you really sure
+	
+		THIRD=`$CD yesno-msgbox --no-cancel --string-output --no-newline --text "Are you really sure you want re-format the disk? All data on this drive will be erased after you click yes."`
+
+		if [ "$THIRD" == "YES" ]; then
+			echo "Uesr clicked yes third time"
+		else
+			echo "User clicked no third time"
+			exit 1
+		fi
+
+
+		#diskutil eraseDisk JHFS+ $TMDselected $DISK
+		echo "drive was formated Journaled HFS+ and repartitioned to GUID"
+		
 else
 	
-	echo "User Selected " $TMD " as their Time Machine Drive"
-	
-	TMDRECORD=`diskutil info $TMD | grep "Partition Type:" | awk '{print $3}'`
-	TMDFORMAT=`diskutil info $TMD | grep "File System Personality:" | awk '{print $4 $5}'`
-	DISK=`diskutil info $TMD | grep "Part of Whole:" | awk '{print $4}'`
-	COREDISK=`diskutil corestorage info $DISK | grep "Conversion Status:" | awk '{print $3}'`
-	
-	echo "Time Machine Drive Selected Partition Type: " $TMDRECORD
-	echo "Time Machine Drive Selected File System Personality: " $TMDFORMAT
-	echo "Time Machine Drive Selected Part of Disk: " $DISK
-	
-	if [ "$COREDISK" == "Complete" ]; then
-		echo "Disk is already encrypted"
-		
-		echo "tmutil setdestination"
-		tmutil setdestination /Volumes/$TMD
+	if [ "$TMDFORMAT" != "JournaledHFS+" ] ; then
+		#ask the users are they really sure
+		#ask the users are they really sure
+		FIRST=`$CD yesno-msgbox --no-cancel --string-output --no-newline --text "The drive you have selected needs to be re-formatted to be used with Time Machine. Do you want to re-format it?"`
 
-		echo "tmutil enable for automatic backups"
-		tmutil enable
-
-		echo "tmutil enable local snapshots"
-		tmutil enablelocal
-		
-		exit 0
-	else
-		echo "Disk is not already encrypted"
-	fi
-	
-	
-	if [ "$TMDRECORD" != "Apple_HFS" ] ; then
-		
-			#ask the users are they really sure
-			FIRST=`/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "The drive you have selected needs to be re-formatted to be used with Time Machine. Do you want to re-format it?" buttons {"Yes","No"} default button 2
-			    end tell
-			EOF`
-
-			if [ "$FIRST" == "$YES" ]; then
-				echo "Uesr clicked yes first time"
-			else
-				echo "User clicked no first time"
-				exit 1
-			fi
-
-			#do you want me to format X
-		
-			SECOND=`/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "All data on this drive will be lost when it is re-formatted. Are you sure you want it re-formatted?" buttons {"Yes","No"} default button 2
-			    end tell
-			EOF`
-
-			if [ "$SECOND" == "$YES" ]; then
-				echo "Uesr clicked yes second time"
-			else
-				echo "User clicked no second time"
-				exit 1
-			fi
-
-			#are you really sure
-		
-			THIRD=`/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "Are you really sure you want re-format the disk? All data on this drive will be erased after you click yes." buttons {"Yes","No"} default button 2
-			    end tell
-			EOF`
-
-			if [ "$THIRD" == "$YES" ]; then
-				echo "Uesr clicked yes third time"
-			else
-				echo "User clicked no third time"
-				exit 1
-			fi
-
-			#are you really really sure
-		
-			#FOURTH=`/usr/bin/osascript <<-EOF
-			#    tell application "System Events"
-			#        activate
-			#        display dialog "Are you REALLY REALLY sure you want me to erase everything on this drive? There is NO going back after click yes." buttons {"Yes","No"} default button 2
-			#    end tell
-			#EOF`
-
-			#if [ "$FOURTH" == "$YES" ]; then
-			#	echo "Uesr clicked yes fourth time"
-			#else
-			#	echo "User clicked no fourth time"
-			#	exit 1
-			#fi
-		
-			diskutil eraseDisk JHFS+ $TMD $DISK
-			echo "drive was formated Journaled HFS+ and repartitioned to GUID"
-			
-	else
-		
-		if [ "$TMDFORMAT" != "JournaledHFS+" ] ; then
-			#ask the users are they really sure
-			FIRST=`/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "The drive you have selected needs to be re-formatted to be used with Time Machine. Do you want to re-format it?" buttons {"Yes","No"} default button 2
-			    end tell
-			EOF`
-
-			if [ "$FIRST" == "$YES" ]; then
-				echo "Uesr clicked yes first time"
-			else
-				echo "User clicked no first time"
-				exit 1
-			fi
-
-			#do you want me to format X
-		
-			SECOND=`/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "All data on this drive will be lost when it is re-formatted. Are you sure you want it re-formatted?" buttons {"Yes","No"} default button 2
-			    end tell
-			EOF`
-
-			if [ "$SECOND" == "$YES" ]; then
-				echo "Uesr clicked yes second time"
-			else
-				echo "User clicked no second time"
-				exit 1
-			fi
-
-			#are you really sure
-		
-			THIRD=`/usr/bin/osascript <<-EOF
-			    tell application "System Events"
-			        activate
-			        display dialog "Are you really sure you want re-format the disk? All data on this drive will be erased after you click yes." buttons {"Yes","No"} default button 2
-			    end tell
-			EOF`
-
-			if [ "$THIRD" == "$YES" ]; then
-				echo "Uesr clicked yes third time"
-			else
-				echo "User clicked no third time"
-				exit 1
-			fi
-
-			#are you really really sure
-		
-			#FOURTH=`/usr/bin/osascript <<-EOF
-			#    tell application "System Events"
-			#        activate
-			#        display dialog "Are you REALLY REALLY sure you want me to erase everything on this drive? There is NO going back after click yes." buttons {"Yes","No"} default button 2
-			#    end tell
-			#EOF`
-
-			#if [ "$FOURTH" == "$YES" ]; then
-			#	echo "Uesr clicked yes fourth time"
-			#else
-			#	echo "User clicked no fourth time"
-			#	exit 1
-			#fi
-		
-			diskutil erase $TMD JHFS+
-			echo "Partition reformated to JHFS+"
-			
+		if [ "$FIRST" == "YES" ]; then
+			echo "Uesr clicked yes first time"
 		else
-		
-			echo "drive is Journaled HFS+ and GUID partitioned"
-	
+			echo "User clicked no first time"
+			exit 1
 		fi
+
+		#do you want me to format X
+	
+		SECOND=`$CD yesno-msgbox --no-cancel --string-output --no-newline --text "All data on this drive will be lost when it is re-formatted. Are you sure you want it re-formatted?"`
+
+		if [ "$SECOND" == "YES" ]; then
+			echo "Uesr clicked yes second time"
+		else
+			echo "User clicked no second time"
+			exit 1
+		fi
+
+		#are you really sure
+	
+		THIRD=`$CD yesno-msgbox --no-cancel --string-output --no-newline --text "Are you really sure you want re-format the disk? All data on this drive will be erased after you click yes."`
+
+		if [ "$THIRD" == "YES" ]; then
+			echo "Uesr clicked yes third time"
+		else
+			echo "User clicked no third time"
+			exit 1
+		fi
+		
+		#diskutil erase $TMDselected JHFS+
+		echo "Partition reformated to JHFS+"
+		
+	else
+	
+		echo "drive is Journaled HFS+ and GUID partitioned"
+
 	fi
+fi
+
+#
 PASSONE="1"
 PASSTWO="2"
 
@@ -219,37 +165,27 @@ while [ $PASSONE != $PASSTWO ];do
 
 
 	#Sets the nothing variable 
-	
 	NOTHING1="1"
+	
 	#input from user on password for the drive
-	
 	while [ $PASSONE -eq $NOTHING1 ]; do
-		PASSONE=`/usr/bin/osascript <<-EOF
-		    tell application "System Events"
-		        activate
-				display dialog "Enter password for Time machine Drive " default answer "1" buttons {"Continue…"} default button 1
-				set theAnswer to (text returned of result)
-		    end tell
-		EOF`
-	done
-
-	#echo $PASSONE
 	
-	NOTHING2="2"
-	#verify password from the user for the drive
-		while [ $PASSTWO -eq $NOTHING2 ]; do
-		PASSTWO=`/usr/bin/osascript <<-EOF
-		    tell application "System Events"
-		        activate
-				display dialog "Re-enter password for Time machine Drive " default answer "2" buttons {"Continue…"} default button 1
-				set theAnswer to (text returned of result)
-		    end tell
-		EOF`
+		PASSONE=`$CD secure-standard-inputbox --no‑cancel --float --informative‑text "Enter password for Time machine Drive:"`
+	
 	done
-
-	#echo $PASSTWO
+	
+	#Sets the nothing2 variable 
+	NOTHING2="2"
+	
+	#verify password from the user for the drive
+	while [ $PASSTWO -eq $NOTHING2 ]; do
+	
+		PASSTWO=`$CD secure-standard-inputbox --no‑cancel --float --informative‑text "Re-enter password for Time machine Drive "` 
+	
+	done
 
 	if [ $PASSONE != $PASSTWO ]; then
+
 		PASSONE="1"
 		PASSTWO="2"
 		
@@ -264,19 +200,16 @@ echo "Users password verified and beginning encryption"
 
 #Convert the drive to encrypted
 echo "diskutil command"
-#diskutil cs convert /Volumes/$TMD -passphrase $PASSONE  
+#diskutil cs convert /Volumes/$TMDselected -passphrase $PASSONE  
 
 #setup time machine to use the drive
 echo "tmutil setdestination"
-#tmutil setdestination /Volumes/$TMD
+#tmutil setdestination /Volumes/$TMDselected
 
 echo "tmutil enable for automatic backups"
 #tmutil enable
 
 echo "tmutil enable local snapshots"
 #tmutil enablelocal
-
-
-fi
 
 exit 0
